db7bb2dee54dce2f54550ef4b570205bb5d33cf1
Andy Dwi Rahmansyah <king@andyy.my.id>
2026-04-29 01:02:31 +0700

    Update globals.h

--- include/globals.h
+++ include/globals.h
@@ -2,11 +2,20 @@
 #define GLOBALS_H
 
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "photo_data.h" // WAJIB biar ikon kedetect
 #include "esp_wifi.h"
 
-// --- OBJEK DISPLAY (Biar semua file bisa gambar ke layar) ---
+// --- SETTING LAYAR ---
 extern Adafruit_SSD1306 display;
+
+// --- PIN JOYSTICK ---
+#define PIN_UP    42
+#define PIN_DOWN  41
+#define PIN_LEFT  40
+#define PIN_RIGHT 39
+#define PIN_OK    38
 
 // --- STRUKTUR DATA ---
 struct WiFiData {
@@ -18,21 +27,36 @@ struct WiFiData {
   String mac;
 };
 
-// --- VARIABEL GLOBAL (Gunakan 'extern') ---
+// --- VARIABEL GLOBAL ---
 extern int appMode;
+extern bool inSubMenu;
+extern int currentMenu;
+extern int currentSub;
+extern int topMenu;
 extern bool isDeauthing;
+extern bool deauthUdahSetup;
 extern bool isSpamming;
 extern int aktifModeSpam;
-extern WiFiData targetTerkunci;
+extern int spamState;
+extern int brightnessValue;
 extern WiFiData listWiFi[30];
+extern WiFiData targetTerkunci;
 extern int totalWiFi;
-extern bool deauthUdahSetup;
+extern int scannerState; // Tambahin ini karena di log error ada scannerState
 
-// --- PROTOTYPE FUNGSI (Biar file lain bisa panggil fungsi ini) ---
+// --- PROTOTYPE FUNGSI (Biar RootX.cpp bisa panggil file lain) ---
 void handleJoystick();
 void tampilkanMenuLogo();
+void tampilkanMenuUtama();
+void tampilkanWifiScanner();
 void loopWiFi(void * pvParameters);
-void sendBeacon(String ssid);
+void tampilkanLogoDulu();
+void tampilkanIntroAnime();
+void tampilkanTeksSplash();
+void handleNavigasiScanner(String btn);
+void handleNavigasiDeauth(String btn);
+void handleNavigasiSpam(String btn);
 void setOledBrightness(uint8_t level);
+void ketikTeks(String teks, int x, int y);
 
 #endif
